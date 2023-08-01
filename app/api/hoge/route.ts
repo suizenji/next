2dc6f18ad2a98f75f5e7f14a8fc09244894f1b2c
vagrant <vagrant@localhost>
2023-08-01 16:22:12 +0900

    api-with-sleep

--- app/api/hoge/route.ts
+++ app/api/hoge/route.ts
@@ -1,6 +1,13 @@
 // https://nextjs.org/docs/app/building-your-application/routing/route-handlers
 import { NextResponse } from 'next/server'
 
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, 2000));
+}
+
 export async function GET() {
   return NextResponse.json({name: 'John Doe'})
+
+  return delay()
+    .then(() => NextResponse.json({name: 'John Doe'}));
 }
